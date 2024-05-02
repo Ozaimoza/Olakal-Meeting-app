b9a0b8e4a2b0b9dd61daa67d76a1fad5877bd408
Mozaik Ramadhan <142678727+Ozaimoza@users.noreply.github.com>
2024-05-02 15:04:52 +0700

    update

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -104,9 +104,7 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
                   }
             }
             link={
-              type === "recordings"
-                ? meeting.url
-                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`
+              type === "recordings" ? meeting.url : `/meeting/${meeting.id}`
             }
           />
         ))
