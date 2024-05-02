3f379b2c40aff2e7527acb69f8c4216e87b587fc
Mozaik Ramadhan <142678727+Ozaimoza@users.noreply.github.com>
2024-05-02 11:37:19 +0700

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+import { Metadata } from "next";
+import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Olakal",
+  description: "Video Calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
